2ee19e9d04c9924a8dbc70e7aec3d2dc3531c661
rights-uchida <uchida@rights-s.jp>
2023-02-24 09:22:15 +0900

    add swagger document for notes

--- app/Http/Controllers/Api/NotesController.php
+++ app/Http/Controllers/Api/NotesController.php
@@ -11,6 +11,31 @@ class NotesController extends Controller
     /**
      * Display a listing of the resource.
      *
+     * @OA\Get(
+     *     path="/api/notes/{id}",
+     *     tags={"Notes"},
+     *     summary="ノート情報を取得する",
+     *     description="ノート情報を取得する",
+     *     @OA\Parameter(
+     *         name="id",
+     *         in="path",
+     *         description="ユーザーID",
+     *         required=true,
+     *         @OA\Schema(
+     *             type="integer",
+     *             format="int64"
+     *         )
+     *     ),
+     *     @OA\Response(
+     *         response=200,
+     *         description="ユーザー情報",
+     *         @OA\JsonContent(ref="#/components/schemas/Note")
+     *     ),
+     *     @OA\Response(
+     *         response=404,
+     *         description="指定したIDのユーザーが見つかりませんでした"
+     *     )
+     * )
      * @return \Illuminate\Http\Response
      */
     public function index()
@@ -42,11 +67,23 @@ class NotesController extends Controller
     }
 
     /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Models\Note  $note
-     * @return \Illuminate\Http\Response
+     * @OA\Put(
+     *     path="/api/notes/{id}",
+     *     summary="Updates a note",
+     *     tags={"Notes"},
+     *     @OA\Parameter(
+     *         description="Parameter with mutliple examples",
+     *         in="path",
+     *         name="id",
+     *         required=true,
+     *         @OA\Schema(type="string"),
+     *         @OA\Examples(example="int", value="1", summary="An int value."),
+     *     ),
+     *     @OA\Response(
+     *         response=200,
+     *         description="OK"
+     *     )
+     * )
      */
     public function update(Request $request, Note $note)
     {
--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -7,6 +7,14 @@ use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
 
+/**
+ * This API is sample.
+ * @OA\Info(
+ *     title="サンプルAPI",
+ *     version="0.1.0"
+ * )
+ * @OA\PathItem(path="/api")
+ */
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
